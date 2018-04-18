189cb660781e36e36dc28495c437a1ee88fb23c5
Yanko <YankoFutekov@gmail.com>
2018-04-18 13:48:17 +0300

    Handle auth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb://Yanko:wolverin3@ds147459.mlab.com:47459/storytime-dev',
+  googleClientID: '57166416495-1fjj2acp3gbpdp8qa52i846hvk2dklvb.apps.googleusercontent.com',
+  googleCLientSecret: 'dhunLKLJkW47799kHdZHSgZ8'
+}
